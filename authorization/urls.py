d58ec4f77f1e6afd290ac72e3fb886937b3e04c0
Yelyzaveta <yel.miroshchenko@gmail.com>
2021-10-08 19:25:33 +0300

    added authorization ?not tested?

--- authorization/urls.py
+++ authorization/urls.py
@@ -1,10 +1,13 @@
 from django.contrib.auth import views as auth_views
 
-from django.urls import path
+from django.urls import path, include
 
-from .views import RegisterUserForm
+from .views import RegisterUserForm, UserUpdate
 
 
 urlpatterns = [
     path('register/', RegisterUserForm.as_view(), name='register'),
+    path('profile/edit/', UserUpdate.as_view(), name='user_update'),
+    path('accounts/', include('django.contrib.auth.urls')),
+]
 ]
--- authorization/views.py
+++ authorization/views.py
@@ -1,23 +1,58 @@
 from django.shortcuts import render
 
 from django.contrib.auth.models import User
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate, login
 
 from .forms import RegistrationForm
 
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView, UpdateView
 
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 def home(request):
-    return render(request, 'base.html')
+    return render(request, 'homepage.html')
+
 
 class RegisterUserForm(SuccessMessageMixin, CreateView):
     model = User
-    template_name = ''
+    template_name = 'register.html'
     form_class = RegistrationForm
-    success_url = ''
+    success_url = 'homepage.html'
     success_message = 'Successfully created!'
 
+    def form_valid(self, form):
+        user = form.save()
+
+        username = self.request.POST['username']
+        password = self.request.POST['password']
+
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return super(RegisterUserForm, self).valid(form)
+
+
+class UserUpdate(LoginRequiredMixin, UpdateView):
+    login_url = 'login'
+    model = User
+    fields = ['firstname', 'lastname', 'email']
+    template_name = 'user_update.html'
+
+    def get_object(self, queryset=None):
+        user = self.request.user
+        return user
+
+
+class UserProfileView(LoginRequiredMixin, DetailView):
+    model = User
+    template_name = 'user_profile.html'
+
+    def get_object(self, queryset=None):
+        user = self.request.user
+        return user
+
+
+class UserPublicProfileView(DetailView):
+    model = User
+    template_name = 'user_public_profile.html'
