af4a0c999a23b964eaa97f2c5367a8ff5468361e
Yelyzaveta <yel.miroshchenko@gmail.com>
2021-10-09 11:11:23 +0300

    added settings_dev

--- practice/settings_dev.py
+++ practice/settings_dev.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'eey!0qgo-$wd5b1ao1=jy!%y522y^0et)r%3m#2@zr5cln5(_w')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -122,8 +122,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-
-# Override production variables if DJANGO_DEVELOPMENT env variable is set
-if os.environ.get('DJANGO_DEVELOPMENT'):
-    from settings_dev import *  # or specific overrides
